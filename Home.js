e076ecdecc7ea68af637bf03d99c8a6418935a90
Sanyam243 <129854450+Sanyam243@users.noreply.github.com>
2023-07-09 11:56:46 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <>
+    <main>
+    <div className="bgimage"><img src="Frame 159.jpg" alt="" /></div>
+        <div className="largetext">
+            Finance & Economics
+        </div>
+        <div className="largetext2"> Finance & Economics</div>
+      
+          <div className="buttons">
+            
+              <button type="button" className=" btn but1" id="but1">JOIN US </button>
+            
+           <button className=" btn but2" type="submit" id="but2">EXPLORE</button>
+          
+          
+          <div className="buttontxt">
+            FEC functions as a platform for students to come together and learn the intriguing and fun world of finance.
+          </div>
+          
+        </div>
+        
+
+    </main>
+    
+    <div className="motoblock">
+      <div className="moto">OUR MOTTO:</div>
+    </div>
+    <div className="mototxt">
+      Be an asset Not a liability
+    </div>
+
+    </>
+  )
+}
