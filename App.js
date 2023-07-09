ba2ab3a64a4f77e91efa4b9a5ce25c71c3076d65
Sanyam243 <129854450+Sanyam243@users.noreply.github.com>
2023-07-09 11:54:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import './home.css';
+
+
+import {Navbar} from "./MyComponents/Navbar";
+import {Home} from "./MyComponents/Home";
+
+
+function App() {
+  return (
+   
+   <>
+    <Navbar/>
+    <Home/>
+ 
+   </>
+  );
+}
+
+export default App;
